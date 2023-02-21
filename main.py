671cbb682db304f7fc955b1c9ccf9673e3c6d67f
rejdysan <michal.rejduga@gmail.com>
2023-02-21 15:24:52 +0100

    Testing pydantic validation

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
+class Festival(BaseModel):
+    title: str
+    description: str
+    includes_food: bool = True
+    rating: Optional[int] = None
+
+@app.get("/hello")
+def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "Posts"}
+
+@app.post("/create")
+def create(festival: Festival):
+    return festival
